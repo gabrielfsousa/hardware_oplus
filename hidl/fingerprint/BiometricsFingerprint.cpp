ece10640a2b8674c5936ec63b7adacdce0ff1534
Gabriel Sousa <ferreiragabrielsousa@gmail.com>
2022-06-13 00:26:31 +0100

    Add blank callbacks for udfps

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -150,6 +150,14 @@ Return<void> BiometricsFingerprint::onFingerprintCmd(int32_t /*cmdId*/,
     return Void();
 }
 
+Return<void> BiometricsFingerprint::onShowUdfpsOverlay() {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onHideUdfpsOverlay() {
+    return Void();
+}
+
 }  // namespace implementation
 }  // namespace V2_3
 }  // namespace fingerprint
