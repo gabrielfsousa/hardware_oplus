3987993f6c802bd77646e58838aaab6de2f55152
vjspranav <pranavasri@live.in>
2022-06-13 08:36:47 +0200

    Revert "vibrator: Implement Oplus effects"
    This reverts commit b64c8238ac6e96e852c0cb1e6025ab9645ddb937.

--- aidl/vibrator/include/Vibrator.h
+++ aidl/vibrator/include/Vibrator.h
@@ -58,11 +58,10 @@ class LedVibratorDevice {
 public:
     LedVibratorDevice();
     int on(int32_t timeoutMs);
-    int onWaveform(int waveformIndex);
     int off();
     bool mDetected;
+private:
     int write_value(const char *file, const char *value);
-    int write_value(const char *file, int value);
 };
 
 #ifdef TARGET_SUPPORTS_OFFLOAD
